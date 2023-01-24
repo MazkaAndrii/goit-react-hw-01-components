8e71910c5da04ffa81cc125370840c4de3eec393
Mazka Andrii <andriy.mazka@gmail.com>
2023-01-24 09:20:34 +0200

    add propTypes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 export default function Profile({ username, tag, location, avatar, stats }) {
   return (
     <div class="profile">
@@ -25,3 +26,15 @@ export default function Profile({ username, tag, location, avatar, stats }) {
     </div>
   );
 }
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
