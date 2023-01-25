1891c989ea0c555e7dc6ded082e7737286fd4c65
Mazka Andrii <andriy.mazka@gmail.com>
2023-01-25 17:30:34 +0200

    add Stat and Tra

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
 export default function Statistics({ title, stats }) {
   return (
-    <section class="statistics">
-      {title && <h2 class="title">Upload stats</h2>}
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>Upload stats</h2>}
 
-      <ul class="stat-list">
+      <ul className={css.statList}>
         {stats.map(stat => (
-          <li class="item" key={stat.id}>
-            <span class="label">{stat.label}</span>
-            <span class="percentage">{stat.percentage}</span>
+          <li className={css.item} key={stat.id}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}</span>
           </li>
         ))}
       </ul>
